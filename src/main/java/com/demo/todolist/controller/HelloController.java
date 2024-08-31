1734565687eb245403bbe1cc904b3df6782b381c
cash <cashwugeek@icloud.com>
2024-08-31 20:38:48 +0800

    Day 06 @value

--- src/main/java/com/demo/todolist/controller/HelloController.java
+++ src/main/java/com/demo/todolist/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.demo.todolist.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${todo.reminder.threshold}")
+    private int reminderThreshold;
+
+    @Value("${todo.reminder.message}")
+    private String reminderMessage;
+
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/reminder-config")
+    public String getReminderConfig() {
+        return "Threshold: " + reminderThreshold + ", Message: " + reminderMessage;
     }
 }
